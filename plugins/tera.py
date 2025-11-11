f7ed51bad2aaf07146822ff84315d85be264a5e3
Broblox124 <140059637+Broblox124@users.noreply.github.com>
2025-11-11 14:11:22 +0530

    Update tera.py

--- plugins/tera.py
+++ plugins/tera.py
@@ -27,7 +27,7 @@ last_upload_col = db["terabox_lastupload"]
 
 TERABOX_REGEX = r'https?://(?:www\.)?[^/\s]*tera[^/\s]*\.[a-z]+/s/[^\s]+'
 
-COOKIE = "ndus=YzrYlCHteHuixx7IN5r0fc3sajSOYAHfqDoPM0dP" # add your own cookies like ndus=YzrYlCHteHuixx7IN5r0ABCDFXDGSTGBDJKLBKMKH
+COOKIE = "ndus=YfMjGd9peHuiCNXDDN-XZo7gqMIEDpy0X4J3VIGX" # add your own cookies like ndus=YzrYlCHteHuixx7IN5r0ABCDFXDGSTGBDJKLBKMKH
 
 HEADERS = {
     "Accept": "application/json, text/plain, */*",
